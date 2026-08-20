860a4e1bf76c733fa0ddd78f77ba13c2ca4103d7
Sakshi kirola <kirolasakshi@gmail.com>
2026-08-20 22:19:48 +0530

    Added history

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Sparkles ,Plus, Menu, X, Bell, CircleUserRound } from "lucide-react"; 
+import {Sparkles ,Plus, Menu, X, Bell, CircleUserRound, CheckCircle, CircleAlert } from "lucide-react"; 
 import { dashboardStats, performanceData, recentInterviews } from "./DashboardData";
 import {LineChart, Line, XAxis, YAxis, CartesianGrid,Tooltip,} from "recharts";
 
@@ -53,8 +53,8 @@ export const Dashboard = () => {
        {/* graph */}
        <div className="bg-[#111827] rounded-xl p-4">
         <div className="flex justify-between items-center mb-2">
-         <h2 className="text-white text-sm font-semibold">Performance Overview</h2>
-         <button className="text-xs text-gray-400 border border-gray-700 rounded-md px-2 py-1">
+         <h2 className="text-white text-lg font-semibold">Performance Overview</h2>
+         <button className="text-sm text-gray-400 border border-gray-700 rounded-md px-2 py-1">
           This Month
          </button>
        </div>
@@ -78,18 +78,48 @@ export const Dashboard = () => {
        </LineChart>
       </div>
 
-       {/* history */}
-       <div>
-        {recentInterviews.map((interview) => (
-         <div key={interview.title}>
-          <h3>{interview.title}</h3>
-          <p>{interview.date}</p>
-          <span>{interview.score}</span>
-         </div>
-        ))}
-       </div>
+      {/* history */}
+      <div className='text-white bg-[#111827] rounded-xl p-4 w-90'>  
+       <div className='flex justify-between items-center mb-4'>  
+        <h1 className="text-white text-lg font-semibold">Recent Interviews</h1>  
+        <p className='text-sm font-semibold text-purple-600 cursor-pointer'>View All</p>  
+       </div>  
+       <div className='space-y-4'>  
+       {recentInterviews.map((interview) => (  
+       <div className='flex justify-between items-center' key={interview.title}>  
+        <div>  
+          <h3 className='text-sm font-medium'>{interview.title}</h3>  
+          <p className='text-gray-400 text-xs mt-1'>{interview.date}</p>  
+        </div>  
+        <div className='text-right'>  
+          <span className='text-purple-400 font-semibold text-sm'>{interview.score}</span>  
+          <p className='text-gray-500 text-xs mt-1'>Score</p>  
+        </div>  
+       </div>  
+       ))}  
+       </div>  
       </div>
 
+      {/* short result */}
+       <div className='flex flex-col gap-4'> 
+        <div className='text-white bg-[#111827] rounded-xl p-4'> 
+        <div className='flex items-center gap-2 mb-2'>
+         <CheckCircle className='w-5 h-5 text-green-500' />
+         <h1 className='text-green-500'>Strengths</h1> 
+        </div>
+        <p>Problem solving, Javascript, React</p> 
+        </div> 
+
+        <div className='text-white bg-[#111827] rounded-xl p-4'> 
+        <div className='flex items-center gap-2 mb-2'>
+         <CircleAlert className='w-5 h-5 text-red-500' />
+         <h1 className='text-red-500'>Areas to Improve</h1> 
+        </div>
+        <p>System Design, DBMS</p>  
+        </div> 
+       </div>
+
+      </div>
     </div>
   )
 }
