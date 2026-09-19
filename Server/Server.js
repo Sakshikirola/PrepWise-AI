af47efdc1b4eeb65a70e4f1452a0caea3eccdd7a
Sakshi kirola <kirolasakshi@gmail.com>
2026-09-19 12:37:18 +0530

    changes in paackage.json

--- Server/Server.js
+++ Server/Server.js
@@ -127,6 +127,8 @@ app.post("/api/evaluate-interview", async (req, res) => {
   }
 });
 
-app.listen(5001, () => {
-  console.log("Server running on http://localhost:5001");  
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });    
